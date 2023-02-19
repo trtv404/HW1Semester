178383c67604890b92b3ce798bb44f989db88bfc
trtv404 <tr_tatjana@mail.ru>
2023-02-19 14:17:05 +0700

    task is done

--- Program.cs
+++ Program.cs
@@ -1,3 +1,10 @@
 ﻿// Задача. Итоги блока. Выбор специализации.
-using static System Console;
+using static System.Console;
 
+Write("введите строку: ");
+string[] inputString = ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+for (int i = 0; i < inputString.Length; i++)
+{
+    if (inputString[i].Length <= 3) Write($"{inputString[i]}, ");
+}
